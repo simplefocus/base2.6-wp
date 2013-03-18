23b8bc5343fded9f77b90ee4278989ad3ac23f76
Casey Zumwalt <casey@simplefocus.com>
2013-03-18 10:19:26 -0500

    Initial commit

---
+++ lib/custom.php
@@ -0,0 +1,21 @@
+<?php 
+// Custom Functions
+
+// Remove the admin bar from the front end
+add_filter( 'show_admin_bar', '__return_false' );
+
+
+// Remove the version number of WP
+// Warning - this info is also available in the readme.html file in your root directory - delete this file!
+remove_action('wp_head', 'wp_generator');
+
+
+// Obscure login screen error messages
+function wpfme_login_obscure(){ return '<strong>Error</strong>: Either the username or password is incorrect.';}
+add_filter( 'login_errors', 'wpfme_login_obscure' );
+
+
+// Disable the theme / plugin text editor in Admin
+define('DISALLOW_FILE_EDIT', true);
+
+?>
